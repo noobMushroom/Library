12b87509cfa104d7b15d776179fd13fa40682355
mushroom <mushroom019.coder@gmail.com>
2022-09-27 21:17:29 +0530

    better version of class

--- script.js
+++ script.js
@@ -1,57 +1,76 @@
-let myArray = [];
+// todo 1) add function to check whether the user read that book or not
+// todo 2) add a function to edit the content of the book.
+
+let bookArray = [];
 class Book {
     constructor(name, author, pages) {
         this.name = name;
         this.author = author;
-        this.pages = pages
-        return (myArray.push({ name: this.name, author: this.author, pages: this.pages }))
+        this.pages = pages;
     }
 }
 
-const show = document.createElement('div')
 const container = document.querySelector('.container');
-const createBtn = document.getElementById("create")
-const deleteBtn = document.getElementById('delete')
-container.appendChild(show)
+const show = document.createElement('div');
+const createBtn = document.getElementById("create");
+container.appendChild(show);
 
 createBtn.addEventListener('click', addBook);
 
 function addBook() {
-    console.log(myArray)
-    const bookName = document.getElementById('name')
-    const author = document.getElementById('author')
-    const pages = document.getElementById('pages')
-    const newBook = new Book(bookName.value, author.value, pages.value)
-    clear(bookName, author, pages)
-    showBook()
+    const bookName = document.getElementById('name');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    if (bookName.value === '') {
+        return
+    }
+    let newBook= bookName.value
+    newBook = new Book(bookName.value, author.value, pages.value);
+    bookArray.push(newBook);
+    clear(bookName, author, pages);
+    showBook();
 }
 
-function deleteBook(some) {
-    const index = myArray.indexOf(some);
+function deleteBook(bookName) {
+    const index = bookArray.indexOf(bookName);
     if (index > -1) {
-        myArray.splice(index, 1);
+        bookArray.splice(index, 1);
     }
-    showBook()
+    showBook();
 }
+
+
+
+function editBook(bookName) {
+    const index = bookArray.indexOf(bookName);
+    if (index > -1) {
+        bookArray.splice(index, 1);
+    }
+    showBook();
+}
+
 
 
 function showBook() {
+    console.log(bookArray)
+    console.log(`this is expArray ${expArray}`)
+    console.log(expArray)
     show.innerHTML = ''
-    myArray.forEach(element => {
-        const showBookDiv = document.createElement('div')
-        show.appendChild(showBookDiv)
+    bookArray.forEach(element => {
+        const showBookDiv = document.createElement('div');
+        show.appendChild(showBookDiv);
         const deleteBtn = document.createElement('button');
-        show.appendChild(deleteBtn)
-        deleteBtn.innerHTML = 'delete'
-        deleteBtn.classList.add(".deleteBtn")
-        deleteBtn.addEventListener('click', () => deleteBook(element))
-        showBookDiv.innerHTML = `name: ${element.name} || author: ${element.author}, || pages: ${element.pages}`
+        show.appendChild(deleteBtn);
+        deleteBtn.innerHTML = 'Remove Book';
+        deleteBtn.classList.add(".deleteBtn");
+        deleteBtn.addEventListener('click', () => deleteBook(element));
+        showBookDiv.innerHTML = `name: ${element.name} <br> author: ${element.author} <br>  pages: ${element.pages}`;
     });
 }
 
 
 function clear(name, author, pages) {
-    author.value = ''
-    pages.value = ''
-    name.value = ''
+    author.value = '';
+    pages.value = '';
+    name.value = '';
 }
